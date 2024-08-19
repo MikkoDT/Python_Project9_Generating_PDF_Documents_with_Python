df9daacd39a971e46abade867bf2b9c69cea97cc
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-19 22:56:56 +0800

    update

--- code/generate_pdf.py
+++ code/generate_pdf.py
@@ -1,7 +1,11 @@
 from fpdf import FPDF
 
+class PDF(FPDF):
+    def header(self):
+        self.image("logo.png",10,8,33)
 
-pdf = FPDF()
+
+pdf = PDF()
 
 pdf.add_page()
 pdf.set_font("helvetica","B",16)
